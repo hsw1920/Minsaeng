0276b3f0fa8eaf0daff8dbcb3ee480edc5dcb966
hsw1920 <hsw1920@gmail.com>
2024-04-16 22:34:19 +0900

    [Feat/#3] ProfilePhoneNumberCreation UI 및 Validation 구현

--- Minsaeng/Sources/Presentation/Profile/ProfilePhoneNumberCreation/ProfilePhoneNumberCreationViewController.swift
+++ Minsaeng/Sources/Presentation/Profile/ProfilePhoneNumberCreation/ProfilePhoneNumberCreationViewController.swift
@@ -11,6 +11,60 @@ import RxCocoa
 import ReactorKit
 
 final class ProfilePhoneNumberCreationViewController: BaseViewController {
+    deinit {
+        print("deinit!")
+    }
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: .zero)
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.delaysContentTouches = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = {
+        let contentView = UIView()
+        return contentView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "휴대전화를 입력해주세요"
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.alpha = 0.0
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        return label
+    }()
+    
+    private let phoneNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .systemFont(ofSize: 24)
+        textField.autocorrectionType = .no
+        textField.spellCheckingType = .no
+        textField.keyboardType = .numberPad
+        textField.placeholder = "휴대전화를 입력해주세요"
+        return textField
+    }()
+    
+    private let underLine: UIView = {
+        let view = UIView()
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        return view
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("다음", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        return button
+    }()
     
     // MARK: Init
     init(with reactor: ProfilePhoneNumberCreationReactor) {
@@ -22,26 +76,187 @@ final class ProfilePhoneNumberCreationViewController: BaseViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupNavigation()
+        setKeyboardObserver()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        phoneNumberTextField.becomeFirstResponder()
+    }
+    
+    override func setupUI() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        [titleLabel, phoneNumberTextField, nextButton, descriptionLabel].forEach {
+            contentView.addSubview($0)
+        }
+        
+        phoneNumberTextField.addSubview(underLine)
+        underLine.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.width.height.equalToSuperview()
+            $0.edges.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(62)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        
+        phoneNumberTextField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(32)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.height.equalTo(50)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(phoneNumberTextField.snp.bottom).offset(8)
+            $0.leading.equalToSuperview().inset(32)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(62)
+        }
+    }
+    
+    private func setupNavigation() {
+        navigationItem.setHidesBackButton(true, animated: true)
     }
 }
 
 extension ProfilePhoneNumberCreationViewController: View {
-    
     func bind(reactor: ProfilePhoneNumberCreationReactor) {
         bindAction(reactor: reactor)
         bindState(reactor: reactor)
     }
     
     private func bindAction(reactor: ProfilePhoneNumberCreationReactor) {
+        phoneNumberTextField.rx.text.orEmpty
+            .map { ProfilePhoneNumberCreationReactor.Action.textFieldValueChanged($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
+        nextButton.rx.tap
+            .map { ProfilePhoneNumberCreationReactor.Action.buttonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
     }
     
     private func bindState(reactor: ProfilePhoneNumberCreationReactor) {
+        reactor.state
+            .map(\.isActive)
+            .distinctUntilChanged()
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
         
+        reactor.state
+            .map(\.isPresent)
+            .distinctUntilChanged()
+            .filter { $0 }
+            .bind(with: self, onNext: { owner, _ in
+                let reactor = ProfileCompleteCreationReactor()
+                let nextViewController = ProfileCompleteViewController(with: reactor)
+                owner.navigationController?.pushViewController(nextViewController, animated: true)
+            })
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map(\.textFieldState)
+            .distinctUntilChanged()
+            .bind(with: self, onNext: { owner, state in
+                owner.animatingWithState(with: state)
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension ProfilePhoneNumberCreationViewController {
+    private func animatingWithState(with state: TextFieldState) {
+        self.view.layoutIfNeeded()
+        switch state {
+        case .empty:
+            UIView.animate(withDuration: 0.3) {
+                self.nextButton.snp.updateConstraints {
+                    $0.bottom.equalToSuperview()
+                }
+                self.view.layoutIfNeeded()
+            }
+            self.titleLabel.alpha = 0
+            self.underLine.layer.borderColor = UIColor.lightGray.cgColor
+            self.descriptionLabel.text = "본인의 휴대전화 번호를 입력해주세요. (번호만 입력)"
+            self.descriptionLabel.textColor = .lightGray
+            self.nextButton.alpha = 0.0
+        case .none:
+            UIView.transition(with: self.view, duration: 0.3) {
+                self.titleLabel.alpha = 1
+                self.underLine.layer.borderColor = UIColor.systemBlue.cgColor
+                self.view.layoutIfNeeded()
+            }
+            UIView.animate(withDuration: 0.3) {
+                self.nextButton.snp.updateConstraints {
+                    $0.bottom.equalToSuperview()
+                }
+                self.view.layoutIfNeeded()
+            }
+            self.descriptionLabel.text = "본인의 휴대전화 번호를 입력해주세요. (번호만 입력)"
+            self.descriptionLabel.textColor = .lightGray
+        case .success:
+            UIView.transition(with: self.view, duration: 0.3) {
+                self.titleLabel.alpha = 1
+                self.underLine.layer.borderColor = UIColor.systemBlue.cgColor
+                self.nextButton.alpha = 1.0
+                self.view.layoutIfNeeded()
+            }
+            UIView.animate(withDuration: 0.3) {
+                self.nextButton.snp.updateConstraints {
+                    $0.bottom.equalToSuperview().inset(62)
+                }
+                self.view.layoutIfNeeded()
+            }
+            
+            self.descriptionLabel.text = "본인의 휴대전화 번호를 입력해주세요. (번호만 입력)"
+            self.descriptionLabel.textColor = .lightGray
+        case .warning:
+            UIView.transition(with: self.view, duration: 0.3) {
+                self.titleLabel.alpha = 1
+                self.underLine.layer.borderColor = UIColor.systemRed.cgColor
+                self.nextButton.alpha = 0.5
+                self.view.layoutIfNeeded()
+            }
+            
+            self.descriptionLabel.text = "올바르지 않은 휴대전화 번호 형식입니다."
+            self.descriptionLabel.textColor = .systemRed
+        }
+    }
+    
+    private func setKeyboardObserver() {
+        NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+        .compactMap { $0.userInfo }
+        .compactMap { $0[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue }
+        .bind(with: self, onNext: { owner, element in
+            let keyboardHeight = element.cgRectValue.height - owner.view.safeAreaInsets.bottom
+            owner.scrollView.snp.updateConstraints {
+                $0.bottom.equalTo(owner.view.safeAreaLayoutGuide).inset(keyboardHeight - 62)
+            }
+            owner.view.layoutIfNeeded()
+        })
+        .disposed(by: disposeBag)
     }
 }

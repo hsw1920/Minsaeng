215669d0fa66f2c0fd5c9cf09b6c7b3bddb30099
hsw1920 <hsw1920@gmail.com>
2024-05-18 21:50:11 +0900

    [Feat/#17] Button의 State에 따라 backgroundColor 설정할 수 있는 extension 추가

---
+++ Minsaeng/Resources/Extension/UIButton+.swift
@@ -0,0 +1,22 @@
+//
+//  UIButton+.swift
+//  Minsaeng
+//
+//  Created by 홍승완 on 2024/05/18.
+//
+
+import UIKit
+
+extension UIButton {
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setFillColor(color.cgColor)
+        context.fill(CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0))
+
+        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        setBackgroundImage(backgroundImage, for: state)
+    }
+}
